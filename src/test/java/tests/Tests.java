f0e72441e6921e89a40cab62f585280eb5dd4f69
RamzyAB <abfam20@gmail.com>
2022-01-13 00:30:38 +0200

    Changing tests name

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -3,22 +3,23 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import net.bytebuddy.build.Plugin.Factory.UsingReflection.Priority;
 import pages.HomePage;
 
 public class Tests extends TestBase{
 
 	HomePage HomePageObj;
 	
-	@Test()
-	public void TestOne() {
+	@Test(priority=1)
+	public void TestMenuRidersBtn() {
 		HomePageObj = new HomePage(driver);
 		HomePageObj.NavigateToRiders();
 		System.out.println(driver.getTitle());
 		Assert.assertTrue(driver.getTitle().contains("Deliveroo | The Deliveroo Riders Website"));
 	}
 	
-	@Test
-	public void TestTwo() {
+	@Test(priority = 2)
+	public void TestSearch() {
 		HomePageObj = new HomePage(driver);
 		driver.navigate().to(WebURL);
 		HomePageObj.WriteInSearch("london");
